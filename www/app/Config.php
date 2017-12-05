c7e1895674e11c3f2ddb316c28ebd329188030ab
Sergey Kazarin <mechmskv@gmail.com>
2017-12-05 12:54:42 +0300

    tiny stuff

--- www/app/Config.php
+++ www/app/Config.php
@@ -6,8 +6,8 @@ class Config
 	public static function init()
 	{
 		define('DB_CONNECTION', 'mysql:host=localhost;dbname=finance');
-		define('DB_USER', 'otkzstat');
-		define('DB_PASS', 'qroCYV!9');
+		define('DB_USER', 'financeTester');
+		define('DB_PASS', 'qroCYV!941');
 		define('LOG_PATH', '/../logger.log');
 	}
 }
